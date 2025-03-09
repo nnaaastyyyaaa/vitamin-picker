80a982086e3e7235f8c59b6d6f4fde931b63c67e
Anastasia Andrienko <nnaaastyyyaaa06@gmail.com>
2025-03-09 20:20:53 +0200

    Created a server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const { createUser } = require('./users/userControler');
+const { checkUser } = require('./users/userControler');
+
+dotenv.config({ path: './config.env' });
+
+const router = express.Router();
+router.post('/login', createUser);
+router.post('/sign-in', checkUser);
+
+const DB = process.env.DATABASE.replace('<db_password>', process.env.PASSWORD);
+mongoose.connect(DB).then(() => {
+  console.log('DB connection succesful');
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/api', router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Listening requests...');
+});
