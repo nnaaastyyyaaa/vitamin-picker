c9109cde08417f0b9190ccc97cf7a68bf73030a4
Anastasia Andrienko <nnaaastyyyaaa06@gmail.com>
2025-03-10 17:40:21 +0200

    Added features

--- users/user-log-in.js
+++ users/user-log-in.js
@@ -1,9 +1,11 @@
 const labeleMail = document.querySelector('.e-mail');
 const labelUsername = document.querySelector('.username');
-const labelPassword = document.querySelector('.password');
-const labelMessage = document.querySelector('.message');
+const labelPassword1 = document.querySelector('.password1');
+const labelPassword2 = document.querySelector('.password2');
+const labelMessage1 = document.querySelector('.message2');
+const labelMessage2 = document.querySelector('.message1');
 const btnCreate = document.querySelector('.button-create-profile');
-const btnVisible = document.getElementById('togglePassword');
+const btnVisible = document.querySelectorAll('#togglePassword');
 
 const API = 'http://localhost:3000/api/login';
 
@@ -11,32 +13,43 @@ btnCreate.addEventListener('click', async (e) => {
   e.preventDefault();
   const eMail = labeleMail.value;
   const username = labelUsername.value;
-  const password = labelPassword.value;
+  const password = labelPassword1.value;
+  const password1 = labelPassword2.value;
+  if (password === password1) {
+    try {
+      const response = await fetch(API, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ eMail, username, password }),
+      });
 
-  try {
-    const response = await fetch(API, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ eMail, username, password }),
-    });
-
-    if (response.ok) {
-      labelMessage.textContent = 'Successfully registered!';
-      labeleMail.value = '';
-      labelUsername.value = '';
-      labelPassword.value = '';
+      if (response.ok) {
+        labelMessage1.textContent = 'Successfully registered!';
+        labeleMail.value = '';
+        labelUsername.value = '';
+        labelPassword1.value = '';
+        labelPassword2.value = '';
+      }
+    } catch (err) {
+      console.log('Error!');
     }
-  } catch (err) {
-    console.log('Error!');
+  } else {
+    labelMessage2.textContent = 'Password should be identical!';
   }
 });
 
-btnVisible.addEventListener('click', () => {
-  if (labelPassword.type === 'password') {
-    labelPassword.type = 'text';
-  } else if (labelPassword.type === 'text') {
-    labelPassword.type = 'password';
-  }
+btnVisible.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    if (index === 0) {
+      labelPassword1.type === 'password'
+        ? (labelPassword1.type = 'text')
+        : (labelPassword1.type = 'password');
+    } else {
+      labelPassword2.type === 'password'
+        ? (labelPassword2.type = 'text')
+        : (labelPassword2.type = 'password');
+    }
+  });
 });
